8684095e552c459d1ee5940cf81b14914b7a483e
金彰海 <akiumikin@gmail.com>
2023-03-23 11:26:51 +0900

    GitHub Providerを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,35 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from 'next/app'
+
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  function Copyright(props: any) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://github.com/akiumikin">
+          akiumikin
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </SessionProvider>
+    </>
+  )
 }
