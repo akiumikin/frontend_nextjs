d0f1b8a5bbbc6e6884040f7772a065ce8cea8f1a
金彰海 <akiumikin@gmail.com>
2023-05-07 23:17:32 +0900

    viewportの設定を修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* icon利用時は https://pictogrammers.com/library/mdi/ を参考にする */}
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.2.96/css/materialdesignicons.min.css" rel="stylesheet" />
       </Head>
